dbedf229e7a05de0f2c1d35aa353dbb3625884df
OfekGoldstein <ofekgold16@gmail.com>
2024-07-01 15:34:38 +0300

    deleted the calculator

--- App/app.py
+++ App/app.py
@@ -9,8 +9,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = os.getenv('APP_SECRET_KEY')
-
+#app.secret_key = os.getenv('APP_SECRET_KEY')
+app.secret_key = 'zaza7531'
 # MongoDB connection setup
 #user = os.getenv('USER')
 #password = os.getenv('PASSWORD')
