106d05318aa9023cd41662670c907111bd78812e
OfekGoldstein <ofekgold16@gmail.com>
2024-07-07 16:28:47 +0300

    modified test.py

--- App/test.py
+++ App/test.py
@@ -1,10 +1,10 @@
 import pytest
-from app import app_test  # Importing app-test.py instead of app.py
+from app_test import app  # Importing app-test.py instead of app.py
 
 @pytest.fixture
 def client():
-    app_test.config['TESTING'] = True
-    with app_test.test_client() as client:
+    app.config['TESTING'] = True
+    with app.test_client() as client:
         yield client
 
 def test_register(client):
