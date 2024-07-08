ca58d82fdd49632dcab94a60e0c099a8d2686b23
OfekGoldstein <ofekgold16@gmail.com>
2024-07-08 11:05:17 +0300

    modified Jenkinsfile

--- App/app.py
+++ App/app.py
@@ -16,7 +16,7 @@ user = os.getenv('USER')
 password = os.getenv('PASSWORD')
 service = os.getenv('SERVICE')
 database = os.getenv('DATABASE')
-mongo_uri = 'mongodb://ofek:ofek2002@mongodb:27017/?authSource=Final-project'
+mongo_uri = 'mongodb://ofek:ofek2002@mongodb.default.svc.cluster.local:27017/?authSource=Final-project'
 #mongo_uri = f'mongodb://{user}:{password}@{service}:27017/?authSource={database}'
 
 client = MongoClient(mongo_uri)
